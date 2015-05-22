2d3fad6e9c31bbb4a0b012d7d434d80993058cdf
German Escobar <german.escobarc@gmail.com>
2015-05-21 22:08:23 -0500

    Implementa la pila

---
+++ stack.rb
@@ -0,0 +1,21 @@
+class Stack
+  def push(e)
+    @top = Node.new(e, @top)
+  end
+
+  def pop
+    return nil unless @top
+    old_top = @top
+    @top = @top.next_node
+    old_top.element
+  end
+end
+
+class Node
+  attr_reader :element, :next_node
+
+  def initialize(element, next_node)
+    @element = element
+    @next_node = next_node
+  end
+end
