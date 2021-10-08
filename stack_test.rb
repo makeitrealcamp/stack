48a4eab71eb5b7c00d0e4f1f0de5b72364226e2b
miriamdare <91304505+miriamdare@users.noreply.github.com>
2021-10-08 17:33:44 -0500

    Update stack_test.rb

--- stack_test.rb
+++ stack_test.rb
@@ -2,12 +2,4 @@ require "./stack"
 require "minitest"
 require "minitest/autorun"
 
-class TestStack < Minitest::Test
-  def setup
-    @stack = Stack.new
-  end
-
-  def test_that_returns_nil_from_empty_stack
-    assert_nil @stack.pop
-  end
 
